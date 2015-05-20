ad350a140d030c8569f273dc6326dd84fb3592f0
Dan Gorst <daniel.gorst81@googlemail.com>
2015-05-20 22:15:18 +0100

    Updates the test string to match that used in testing
    Also improves logging.

--- radioTester.js
+++ radioTester.js
@@ -14,7 +14,7 @@ app.use(defaultContentTypeMiddleware);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var expectedString = "Test String";
+var expectedString = "$$icarus,722,12:18:36,52.072820,0.250666,28608,19.26,178.8,17.6,-16.6*20\n";
 var goodCount = 0;
 var badCount = 0;
 
@@ -27,7 +27,7 @@ app.all('*', function(req, res) {
             }
             var buffer = new Buffer(base64data, 'base64');
             var decodedDataString = buffer.toString();
-            console.log('Received string: ' + decodedDataString);
+            console.log('Received string: ' + JSON.stringify(decodedDataString));
             if (decodedDataString === expectedString) {
                 ++goodCount;
             }
@@ -36,7 +36,7 @@ app.all('*', function(req, res) {
             }
             var total = goodCount + badCount;
             var successRate = (goodCount / total) * 100;
-            console.log('Success rate: ' + successRate + '%');
+            console.log('Total: ' + total + '. Success rate: ' + successRate + '%');
         }
         res.send('OK');
     } catch(err) {
